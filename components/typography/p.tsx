9d20fb92aae1ab37a00014011436b7c241215b72
Alexandre Trotel <trotelalexandre@proton.me>
2024-07-10 08:30:27 +0200

    fix: build error

--- components/typography/p.tsx
+++ components/typography/p.tsx
@@ -1,3 +1,13 @@
+function getTextSizeClass(size: string): string {
+  if (size === "md") {
+    return "text-md";
+  } else if (size === "lg") {
+    return "text-lg";
+  } else {
+    return "text-md"; // Default case
+  }
+}
+
 export default function TypographyP({
   children,
   muted,
@@ -12,15 +22,7 @@ export default function TypographyP({
   return (
     <p
       className={`${muted ? "text-muted-foreground" : "text-primary-foreground"} ${center && "text-center"} ${
-        size === "xs"
-          ? "text-xs"
-          : size === "sm"
-            ? "text-sm"
-            : size === "md"
-              ? "text-md"
-              : size === "lg"
-                ? "text-lg"
-                : "text-md"
+        size && getTextSizeClass(size)
       }`}
     >
       {children}
