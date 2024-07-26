b4d9e95670eb2bf8e41f4af57b71fcbd86f58cf2
Alexandre Trotel <trotelalexandre@proton.me>
2024-07-26 15:42:55 +0200

    fix: charts

---
+++ utils/date.ts
@@ -0,0 +1,9 @@
+export const getDateRange = (startDate: Date, endDate: Date) => {
+  const dates = [];
+  let currentDate = new Date(startDate);
+  while (currentDate <= endDate) {
+    dates.push(new Date(currentDate));
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+  return dates;
+};
