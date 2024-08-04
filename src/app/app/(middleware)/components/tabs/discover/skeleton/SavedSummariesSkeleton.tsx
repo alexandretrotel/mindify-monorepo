a21979695f8fb2479351e2097f274357e537cfe2
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-04 16:15:49 +0200

    fix: skeletons

--- src/app/app/(middleware)/components/tabs/discover/skeleton/SavedSummariesSkeleton.tsx
+++ src/app/app/(middleware)/components/tabs/discover/skeleton/SavedSummariesSkeleton.tsx
@@ -28,7 +28,7 @@ const SavedSummariesSkeleton = async () => {
         <CarouselContent className="-ml-4">
           {Array.from({ length: 15 }).map((_, index) => {
             return (
-              <CarouselItem key={index} className="pl-4 lg:basis-1/4">
+              <CarouselItem key={index} className="pl-4 md:basis-1/3 lg:basis-1/4">
                 <BookCoverSkeleton />
               </CarouselItem>
             );
