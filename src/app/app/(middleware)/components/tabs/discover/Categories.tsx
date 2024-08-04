0c71243ab838716c85913883743d16f771daefcc
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-04 15:39:17 +0200

    fix: categories

--- src/app/app/(middleware)/components/tabs/discover/Categories.tsx
+++ src/app/app/(middleware)/components/tabs/discover/Categories.tsx
@@ -32,80 +32,101 @@ const Categories = async () => {
     : [];
 
   return (
-    <Carousel
-      opts={{
-        align: "start",
-        slidesToScroll: "auto"
-      }}
-      className="w-full"
-    >
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col">
-          <H3>Vos intérêts</H3>
-          <Muted size="md">
-            {sortedUserTopics?.length > 0
-              ? "Explorez des résumés extraits de vos sujets préférés."
-              : "Explorez des résumés en fonction de certains sujets."}
-          </Muted>
+    <>
+      <Carousel
+        opts={{
+          align: "start",
+          slidesToScroll: "auto"
+        }}
+        className="w-full lg:hidden"
+      >
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col">
+            <H3>Vos intérêts</H3>
+            <Muted size="md">
+              {sortedUserTopics?.length > 0
+                ? "Explorez des résumés extraits de vos sujets préférés."
+                : "Explorez des résumés en fonction de certains sujets."}
+            </Muted>
+          </div>
+
+          <CarouselContent className="-ml-4">
+            {(sortedUserTopics?.length >= 3 ? sortedUserTopics : sortedTopics)
+              ?.reduce((acc, topic, index) => {
+                const chunkIndex = Math.floor(index / 4);
+
+                if (!acc[chunkIndex]) {
+                  acc[chunkIndex] = [];
+                }
+
+                acc[chunkIndex].push(topic as Tables<"topics">);
+                return acc;
+              }, [] as Tables<"topics">[][])
+              .map((topicChunk, index) => (
+                <CarouselItem key={index} className="pl-4">
+                  <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
+                    {topicChunk.map((topic) => (
+                      <Button asChild key={topic.id} className="col-span-1">
+                        <Link href={`/app/topic/${topic.slug}`} className="w-full">
+                          <TopicIcon isChecked={true} topic={topic} />
+                          <H5Span>{topic.name}</H5Span>
+                        </Link>
+                      </Button>
+                    ))}
+                  </div>
+                </CarouselItem>
+              ))}
+          </CarouselContent>
         </div>
+      </Carousel>
 
-        <CarouselContent className="-ml-4 lg:hidden">
-          {(sortedUserTopics?.length >= 3 ? sortedUserTopics : sortedTopics)
-            ?.reduce((acc, topic, index) => {
-              const chunkIndex = Math.floor(index / 4);
+      <Carousel
+        opts={{
+          align: "start",
+          slidesToScroll: "auto"
+        }}
+        className="hidden w-full lg:block"
+      >
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col">
+            <H3>Vos intérêts</H3>
+            <Muted size="md">
+              {sortedUserTopics?.length > 0
+                ? "Explorez des résumés extraits de vos sujets préférés."
+                : "Explorez des résumés en fonction de certains sujets."}
+            </Muted>
+          </div>
 
-              if (!acc[chunkIndex]) {
-                acc[chunkIndex] = [];
-              }
+          <CarouselContent className="-ml-4">
+            {(sortedUserTopics?.length >= 3 ? sortedUserTopics : sortedTopics)
+              ?.reduce((acc, topic, index) => {
+                const chunkIndex = Math.floor(index / 8);
 
-              acc[chunkIndex].push(topic as Tables<"topics">);
-              return acc;
-            }, [] as Tables<"topics">[][])
-            .map((topicChunk, index) => (
-              <CarouselItem key={index} className="pl-4">
-                <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
-                  {topicChunk.map((topic) => (
-                    <Button asChild key={topic.id} className="col-span-1">
-                      <Link href={`/app/topic/${topic.slug}`} className="w-full">
-                        <TopicIcon isChecked={true} topic={topic} />
-                        <H5Span>{topic.name}</H5Span>
-                      </Link>
-                    </Button>
-                  ))}
-                </div>
-              </CarouselItem>
-            ))}
-        </CarouselContent>
+                if (!acc[chunkIndex]) {
+                  acc[chunkIndex] = [];
+                }
 
-        <CarouselContent className="-ml-4 hidden lg:block">
-          {(sortedUserTopics?.length >= 3 ? sortedUserTopics : sortedTopics)
-            ?.reduce((acc, topic, index) => {
-              const chunkIndex = Math.floor(index / 8);
-
-              if (!acc[chunkIndex]) {
-                acc[chunkIndex] = [];
-              }
-
-              acc[chunkIndex].push(topic as Tables<"topics">);
-              return acc;
-            }, [] as Tables<"topics">[][])
-            .map((topicChunk, index) => (
-              <CarouselItem key={index} className="pl-4">
-                <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
-                  {topicChunk.map((topic) => (
-                    <Button asChild key={topic.id} className="col-span-1">
-                      <Link href={`/app/topic/${topic.slug}`} className="w-full">
-                        <TopicIcon isChecked={true} topic={topic} />
-                        <H5Span>{topic.name}</H5Span>
-                      </Link>
-                    </Button>
-                  ))}
-                </div>
-              </CarouselItem>
-            ))}
-        </CarouselContent>
-      </div>
-    </Carousel>
+                acc[chunkIndex].push(topic as Tables<"topics">);
+                return acc;
+              }, [] as Tables<"topics">[][])
+              .map((topicChunk, index) => (
+                <CarouselItem key={index} className="pl-4">
+                  <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
+                    {topicChunk.map((topic) => (
+                      <Button asChild key={topic.id} className="col-span-1">
+                        <Link href={`/app/topic/${topic.slug}`} className="w-full">
+                          <TopicIcon isChecked={true} topic={topic} />
+                          <H5Span>{topic.name}</H5Span>
+                        </Link>
+                      </Button>
+                    ))}
+                  </div>
+                </CarouselItem>
+              ))}
+          </CarouselContent>
+        </div>
+      </Carousel>
+    </>
   );
 };
 
