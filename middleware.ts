a56d1b84a432e212c54e53d496a8dc98b7a3f15e
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-18 17:24:46 +0200

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -18,11 +18,7 @@ export const config = {
      */
     {
       source:
-        "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-      missing: [
-        { type: "header", key: "next-router-prefetch" },
-        { type: "header", key: "purpose", value: "prefetch" }
-      ]
+        "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"
     }
   ]
 };
