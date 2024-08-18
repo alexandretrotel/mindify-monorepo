ea7a184e441612e9ac12fcbac9f1867d815054c4
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-18 17:43:15 +0200

    remove: default

--- src/utils/supabase/default.ts
+++
@@ -1,7 +0,0 @@
-import type { Database } from "@/types/supabase";
-import { createServerClient } from "@supabase/ssr";
-
-export const supabaseDefaultClient = createServerClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
