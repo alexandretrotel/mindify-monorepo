cae0b78411ea1b6281f5205a3ea9de30ad473992
Alexandre Trotel <trotelalexandre@proton.me>
2024-08-30 10:59:21 +0200

    fix: playlist

--- src/components/features/playlist/PlaylistGrid.tsx
+++ src/components/features/playlist/PlaylistGrid.tsx
@@ -1,5 +1,6 @@
 import { getMindsFromPlaylistSlug } from "@/actions/playlists";
 import Mind from "@/components/global/Mind";
+import H2 from "@/components/typography/h2";
 import type { Tables } from "@/types/supabase";
 import type { UUID } from "crypto";
 import { notFound } from "next/navigation";
@@ -21,10 +22,14 @@ const PlaylistGrid = async ({
   }
 
   return (
-    <div className="grid gap-4 md:grid-cols-2">
-      {playlist?.minds?.map((mind) => {
-        return <Mind key={mind.id} mind={mind} isConnected={isConnected} userId={userId} />;
-      })}
+    <div className="flex flex-col gap-4">
+      <H2>{playlist?.title}</H2>
+
+      <div className="grid gap-4 md:grid-cols-2">
+        {playlist?.minds?.map((mind) => {
+          return <Mind key={mind.id} mind={mind} isConnected={isConnected} userId={userId} />;
+        })}
+      </div>
     </div>
   );
 };
--- src/components/features/playlist/skeleton/PlaylistGridSkeleton.tsx
+++ src/components/features/playlist/skeleton/PlaylistGridSkeleton.tsx
@@ -1,7 +1,14 @@
+import MindSkeleton from "@/components/global/skeleton/MindSkeleton";
 import React from "react";
 
 const PlaylistGridSkeleton = async () => {
-  return <div className="grid gap-4 md:grid-cols-2"></div>;
+  return (
+    <div className="grid gap-4 md:grid-cols-2">
+      {Array.from({ length: 6 }).map((_, index) => {
+        return <MindSkeleton key={index} />;
+      })}
+    </div>
+  );
 };
 
 export default PlaylistGridSkeleton;
