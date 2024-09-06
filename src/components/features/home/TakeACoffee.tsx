546bd4618be720749def2780fb2dfa37e397ccb2
Alexandre Trotel <trotelalexandre@proton.me>
2024-09-06 22:53:57 +0200

    fix: text size

--- src/components/features/home/TakeACoffee.tsx
+++ src/components/features/home/TakeACoffee.tsx
@@ -10,7 +10,7 @@ const TakeACoffee = async () => {
     <Section id="take-a-coffee">
       <div className="flex flex-col items-center gap-4 md:gap-8">
         <h2 className="text-center text-2xl font-semibold md:text-4xl">On se prend un café ?</h2>
-        <Button size="lg" className="text-base md:w-fit md:text-lg" asChild>
+        <Button size="lg" className="text-base md:w-fit" asChild>
           <Link target="_blank" href={calendlyLink}>
             Appelle les fondateurs
           </Link>
