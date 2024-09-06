e33d9555b21efabd19a0edc16c08a268126e95ef
Alexandre Trotel <trotelalexandre@proton.me>
2024-09-06 17:59:00 +0200

    fix: coffee

--- src/components/features/home/TakeACoffee.tsx
+++ src/components/features/home/TakeACoffee.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 
-const calendlyLink: string = "";
+const calendlyLink: string = "https://calendly.com/akamirchau/30min";
 
 const TakeACoffee = async () => {
   return (
@@ -11,7 +11,9 @@ const TakeACoffee = async () => {
       <div className="flex flex-col items-center gap-4 md:gap-8">
         <h2 className="text-center text-2xl font-semibold md:text-4xl">On se prend un café ?</h2>
         <Button size="lg" className="w-full px-12 text-lg md:w-fit" asChild>
-          <Link href={calendlyLink}>Obtiens un appel exclusif avec les fondateurs</Link>
+          <Link target="_blank" href={calendlyLink}>
+            Obtiens un appel exclusif avec les fondateurs
+          </Link>
         </Button>
       </div>
     </Section>
